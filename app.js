61bea6cacdf23a5e9b2cd1edbd2c4f13f339ce9c
sharonchen9593 <sharonchen9593@gmail.com>
2017-06-15 15:53:42 -0700

    can now get, post and delete

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController= require('./controllers/setupController');
+var apiController = require('./controllers/apicontroller');
+
 
 var port = process.env.PORT || 3000;
 
@@ -13,6 +15,8 @@ app.set('view engine', 'ejs');
 mongoose.connect(config.getDbConnectionString()); // connects to mongo database
 
 setupController(app);
+
+apiController(app);
 
 app.listen(port);
 
